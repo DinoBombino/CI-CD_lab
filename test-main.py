f399760ba79b830fb927ca9c5a57a8acc76d17a4
dmitriy <korepanov.dmitriy.04@mail.ru>
2025-09-13 14:09:20 +0800

    Initial code in dev

---
+++ test-main.py
@@ -0,0 +1,12 @@
+import unittest
+from main import add
+
+class TestAdd(unittest.TestCase):
+    def test_add_pos(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_neg(self):
+        self.assertEqual(add(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
