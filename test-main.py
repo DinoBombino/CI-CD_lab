203f8e683a2b0139d8150a09ecab9bb08870a9fc
dmitriy <korepanov.dmitriy.04@mail.ru>
2025-09-23 14:36:17 +0800

    Upd autotest

--- test-main.py
+++ test-main.py
@@ -7,6 +7,9 @@ class TestAdd(unittest.TestCase):
         
     def test_add_pos2(self):
         self.assertEqual(add(3, 3), 6)
+        
+    def test_add_pos3(self):
+        self.assertEqual(add(3, 4), 7)
 
     def test_add_neg(self):
         self.assertEqual(add(-1, 1), 0)
