0ea24791414ca0f83eccf0f42654fb858bafebfc
dmitriy <korepanov.dmitriy.04@mail.ru>
2025-09-23 13:55:39 +0800

    Add autotest for test SCM

--- test-main.py
+++ test-main.py
@@ -7,6 +7,9 @@ class TestAdd(unittest.TestCase):
         
     def test_add_pos_2(self):
         self.assertEqual(add(3, 3), 6)
+        
+    def test_add_pos_3(self):
+        self.assertEqual(add(3, 4), 7)
 
     def test_add_neg(self):
         self.assertEqual(add(-1, 1), 0)
